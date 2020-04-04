90ebabbe2f065c9fab33b69b0e0fb0d5eee3dbda
cabell365 <63163405+cabell365@users.noreply.github.com>
2020-04-04 12:47:22 -0600

    Update Example Execution of DBAdmin Utilities.sql

--- SQLServer/DBAdmin/Example Execution of DBAdmin Utilities.sql
+++ SQLServer/DBAdmin/Example Execution of DBAdmin Utilities.sql
@@ -1,15 +1,15 @@
 --Script rebuild indexes with average fragmentation > 30
-EXEC [DB_ADMIN]..get_index_fragmentation @DBNAME = 'GMA', @SCRIPT = 1
+EXEC [DB_ADMIN]..get_index_fragmentation @DBNAME = '<db>', @SCRIPT = 1
 
 --List all tables sorted by average fragmentation
-EXEC [DB_ADMIN]..get_index_fragmentation @DBNAME = 'DELL_MCMDATA', @SCRIPT = 0
+EXEC [DB_ADMIN]..get_index_fragmentation @DBNAME = '<bb>', @SCRIPT = 0
 
 --Lists Indexes that might not be used
-EXEC [DB_ADMIN]..[get_indexes_not_used] @DBNAME = 'GMA'
-EXEC [DB_ADMIN]..[get_indexes_not_used] @DBNAME = 'DELL_MCMDATA'
+EXEC [DB_ADMIN]..[get_indexes_not_used] @DBNAME = '<db>'
+EXEC [DB_ADMIN]..[get_indexes_not_used] @DBNAME = '<db>'
 
 --Get index usuage by database
-EXEC [DB_ADMIN]..[get_index_usage] @DBNAME = 'GMA'
+EXEC [DB_ADMIN]..[get_index_usage] @DBNAME = '<db>'
 
 --Get current memory usage
 EXEC [DB_ADMIN]..[get_current_memory_usage]
@@ -18,22 +18,22 @@ EXEC [DB_ADMIN]..[get_current_memory_usage]
 EXEC [DB_ADMIN]..[get_20_slowest_queries]
 
 --Get execution plan and sql for the current date on a particular database
-EXEC [DB_ADMIN]..[get_query_plan_by_db] @DBNAME = 'GMA'
+EXEC [DB_ADMIN]..[get_query_plan_by_db] @DBNAME = '<db>'
 
 --Get execution plan and sql for a specific date on a particular database
-EXEC [DB_ADMIN]..[get_query_plan_by_db] @DBNAME = 'GMA', @MYDATE = '02/03/2015'
+EXEC [DB_ADMIN]..[get_query_plan_by_db] @DBNAME = '<db>', @MYDATE = '02/03/2015'
 
 --Get active processes
 EXEC [DB_ADMIN]..[get_active_processes]
 
 --Get database table sizes
-exec DB_ADMIN..[get_db_table_sizes] 'gma'
+exec DB_ADMIN..[get_db_table_sizes] '<db>'
 
 --Get database table row counts
-exec DB_ADMIN..[get_db_table_rowcounts] 'gma'
+exec DB_ADMIN..[get_db_table_rowcounts] '<db>'
 
 --Get space free for a database
-exec DB_ADMIN..[get_db_percent_free] 'gma'
+exec DB_ADMIN..[get_db_percent_free] '<db>'
 
 --Get max space free percent check on all database
 exec DB_ADMIN..[get_db_percent_free] 30
